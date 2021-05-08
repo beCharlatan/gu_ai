c95749322a1f765fdb45bc12db256f46751b56dd
Artyom <apetroov@gmail.com>
2021-05-08 16:15:46 +0300

    complete py/task06

---
+++ py/task06/car.py
@@ -0,0 +1,65 @@
+# 4. Реализуйте базовый класс Car. У данного класса должны быть следующие атрибуты: speed, color, name, is_police (
+# булево). А также методы: go, stop, turn(direction), которые должны сообщать, что машина поехала, остановилась,
+# повернула (куда). Опишите несколько дочерних классов: TownCar, SportCar, WorkCar, PoliceCar. Добавьте в базовый
+# класс метод show_speed, который должен показывать текущую скорость автомобиля. Для классов TownCar и WorkCar
+# переопределите метод show_speed. При значении скорости свыше 60 (TownCar) и 40 (WorkCar) должно выводиться
+# сообщение о превышении скорости.
+class Car:
+    def __init__(self, speed, color, name, is_police):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = is_police
+
+    def go(self):
+        print(f'{self.name} goes')
+
+    def stop(self):
+        print(f'{self.name} stops')
+
+    def turn(self, direction):
+        print(f'{self.name} turns {direction}')
+
+    def show_speed(self):
+        print(f'{self.speed} is current speed')
+
+
+class TownCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+    def show_speed(self):
+        if self.speed > 60:
+            print('Превышение скорости!')
+
+
+class SportCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+
+class WorkCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+    def show_speed(self):
+        if self.speed > 40:
+            print('Превышение скорости!')
+
+
+class PoliceCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+
+town_car = TownCar(61, 'grey', 'Skoda', False)
+sport_car = SportCar(120, 'red', 'Ferrari', False)
+work_car = WorkCar(42, 'black', 'Man', False)
+police_car = PoliceCar(80, 'blue', 'Lada', True)
+
+police_car.go()
+police_car.turn('left')
+police_car.stop()
+sport_car.show_speed()
+town_car.show_speed()
+work_car.show_speed()
