023538d41714e073ab9311c9f7231892314bb217
Artyom Petrov <apetroov@gmail.com>
2021-05-23 21:49:10 +0300

    update lesson1

--- pyalgs/lesson1/task06.py
+++ pyalgs/lesson1/task06.py
@@ -7,7 +7,7 @@ if a + b > c and a + c > b and b + c > a:
   print(f'Можно построить треугольник со сторонами {a}, {b}, {c}')
   if a == b and b == c:
     print('Треугольник равносторонний')
-  elif a == b or b == c:
+  elif a == b or b == c or a == c:
     print('Треугольник равнобедренный')
   else:
     print('Треугольник разносторонний')
